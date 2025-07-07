9cd7d9d396c18cd9d7d55b5c76f547f216146979
Brijesh Raman <brijesh.zte@gmail.com>
2025-07-07 11:41:17 +0530

    rgcodeadded1

--- main.tf
+++ main.tf
@@ -2,3 +2,7 @@ resource "azurerm_resource_group" "rg" {
     name = "prashntrg"
     location = "westus"
 }
+resource "azurerm_resource_group" "rgprashnat" {
+    name = "prashntrgp1"
+    location = "westus"
+}
